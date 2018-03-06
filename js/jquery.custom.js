8d7d0c19c24adb8f4c03642f38dfb21645f5c128
ajayraj123 <ajayraj.next@gmail.com>
2018-03-06 23:09:00 +0530

    added scroll to top

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -52,6 +52,31 @@ $(document).ready(function(){
 	// initialize typeMenu
 	// this plugin was specifically written for Typefolio
 	$('#nav').typeMenu();
+    
+      var scrollTop = $(".scrollTop");
+
+  $(window).scroll(function() {
+    // declare variable
+    var topPos = $(this).scrollTop();
+
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+      $(scrollTop).css("opacity", "1");
+
+    } else {
+      $(scrollTop).css("opacity", "0");
+    }
+
+  }); // scroll END
+
+  //Click event to scroll to top
+  $(scrollTop).click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+
+  });
 
 
 });
